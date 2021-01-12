683ae76acb9027e67a4f48e47f0418534fbbe3c7
Hustin <hustinkava@gmail.com>
2021-01-12 12:41:21 -0700

    for loop working

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/burgers', (req, res) => {
-    burger.create('burger_name', req.body.name, (result) => {
+    burger.create('burger_name', [req.body.name], (result) => {
         // Send back the ID of the new quote
         res.json({ id: result.insertId });
     });
